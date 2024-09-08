a36c7eca491f3825bc71925dadca717930c9a161
momaromar <mohamad@omaralassaf.com>
2024-09-08 18:09:40 -0500

    Attempted some troubleshooting steps and could not fix the issue in App.js yet.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,27 @@ import PaymentPage from './components/PaymentPage';
 
 function App() {
   return (
+    
+    /*<div style={styles.app}>
+      <ChatWindow />
+    </div>*/
     <Router>
       <Routes>
         <Route path="/" element={<ChatWindow />} />
         <Route path="/subscribe" element={<PaymentPage />} />
       </Routes>
     </Router>
-  );
+  ); /* It seems that when I uncomment the div at the start and get rid of the entire Router block it works fine... */
 }
+
+const styles = {
+  app: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    backgroundColor: '#f5f5f5',
+  },
+};
 
 export default App;
